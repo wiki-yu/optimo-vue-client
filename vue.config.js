f742f75157bb1854e40afd9fc61b902d54729061
wiki-yu <xyu0127@gmail.com>
2020-10-21 10:24:58 -0500

    add video

--- vue.config.js
+++ vue.config.js
@@ -28,16 +28,36 @@ module.exports = {
   // tweak internal webpack configuration.
   // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
   // 如果你不需要使用eslint，把lintOnSave设为false即可
-  lintOnSave: true,
+  lintOnSave: false,
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
       .set('_c', resolve('src/components'))
   },
   // 设为false打包时不生成.map文件
-  productionSourceMap: false
+  productionSourceMap: false,
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
   // devServer: {
   //   proxy: 'localhost:3000'
   // }
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    stats: {
+      colors: true
+    },
+    proxy: {
+      // 匹配代理的url
+      '/api': {
+        // 目标服务器地址
+        target: 'localhost:5000',
+        // 路径重写
+        pathRewrite: {
+          '^/api': '/api'
+        },
+        changeOrigin: true
+      }
+    }
+  }
 }
